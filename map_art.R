3224df2b745686d57fe0b5c1d6d97b0e606be1fa
Sebastien Chaurin <chaurinsebastien@johndeere.com>
2020-11-04 18:32:34 +0100

    Lint

--- map_art.R
+++ map_art.R
@@ -4,22 +4,22 @@ bbx <- getbb("Orleans France")
 min_lon <- 1.84; max_lon <- 1.99
 min_lat <- 47.81; max_lat <- 47.93
 bbx <- rbind(x = c(min_lon, max_lon), y = c(min_lat, max_lat))
-colnames(bbx) <- c("min","max")
+colnames(bbx) <- c("min", "max")
 
 bbx <- getbb("brisbane australia")
 
 min_lon <- 152.7; max_lon <- 153.5
 min_lat <- -27.7; max_lat <- -27
 bbx <- rbind(x = c(min_lon, max_lon), y = c(min_lat, max_lat))
-colnames(bbx) <- c("min","max")
+colnames(bbx) <- c("min", "max")
 
 highways <- bbx %>%
-  opq()%>%
-  add_osm_feature(key = "highway", 
-                  value=c("motorway", "trunk",
-                          "primary","secondary", 
-                          "tertiary","motorway_link",
-                          "trunk_link","primary_link",
+  opq() %>%
+  add_osm_feature(key = "highway",
+                  value = c("motorway", "trunk",
+                          "primary", "secondary",
+                          "tertiary", "motorway_link",
+                          "trunk_link", "primary_link",
                           "secondary_link",
                           "tertiary_link")) %>%
   osmdata_sf()
@@ -28,28 +28,28 @@ library(ggplot2)
 library(sf)
 ggplot() +
   geom_sf(data = highways$osm_lines,
-          aes(color=highway),
+          aes(color = highway),
           size = .4,
-          alpha = .65)+
+          alpha = .65) +
   theme_void()
 
 streets <- bbx %>%
-  opq()%>%
-  add_osm_feature(key = "highway", 
+  opq() %>%
+  add_osm_feature(key = "highway",
                   value = c("residential", "living_street",
-                            "service","unclassified",
+                            "service", "unclassified",
                             "pedestrian", "footway",
-                            "track","path")) %>%
+                            "track", "path")) %>%
   osmdata_sf()
 
 ggplot() +
   geom_sf(data = streets$osm_lines,
-          aes(color=highway),
+          aes(color = highway),
           size = .4,
-          alpha = .65)+
+          alpha = .65) +
   theme_void()
 
-color_roads <- rgb(0.42,0.449,0.488)
+color_roads <- rgb(0.42, 0.449, 0.488)
 ggplot() +
   geom_sf(data = streets$osm_lines,
           col = color_roads,
@@ -58,39 +58,40 @@ ggplot() +
   geom_sf(data = highways$osm_lines,
           col = color_roads,
           size = .6,
-          alpha = .8)+
-  coord_sf(xlim = c(min_lon,max_lon),
-           ylim = c(min_lat,max_lat),
+          alpha = .8) +
+  coord_sf(xlim = c(min_lon, max_lon),
+           ylim = c(min_lat, max_lat),
            expand = FALSE) +
   theme(legend.position = F) + theme_void()
 
-river <- bbx%>%
-  opq()%>%
+river <- bbx %>%
+  opq() %>%
   add_osm_feature(key = "waterway", value = "river") %>%
   osmdata_sf()
 
-lake <- bbx%>%
-  opq()%>%
+lake <- bbx %>%
+  opq() %>%
   add_osm_feature(key = "water", value = "lake") %>%
   osmdata_sf()
 
-railway <- bbx%>%
-  opq()%>%
-  add_osm_feature(key = "railway", value="rail") %>%
+railway <- bbx %>%
+  opq() %>%
+  add_osm_feature(key = "railway", value = "rail") %>%
   osmdata_sf()
 
 coast_data <- bbx %>%
   opq() %>%
-  add_osm_feature (key = "natural", value = "coastline") %>%
+  add_osm_feature(key = "natural", value = "coastline") %>%
   osmdata_sf()
 
 coast <- osmplotr::osm_line2poly(coast_data$osm_lines, bbox = bbx)
 
-osm_basemap (bbox = bbox) %>%
-  add_osm_objects(coast$sea, col = 'cadetblue2') %>%
-  add_osm_objects(coast$land, col = 'sienna2')
+osm_basemap(bbox = bbox) %>%
+  add_osm_objects(coast$sea, col = "cadetblue2") %>%
+  add_osm_objects(coast$land, col = "sienna2")
 
 ggplot() +
+  # theme_void() +
   geom_sf(data = streets$osm_lines,
           col = color_roads,
           size = .4,
@@ -123,9 +124,8 @@ ggplot() +
           inherit.aes = FALSE,
           color = "black",
           size = .2,
-          linetype="dotdash",
+          linetype = "dotdash",
           alpha = .5) +
-  coord_sf(xlim = c(min_lon,max_lon),
-           ylim = c(min_lat,max_lat),
-           expand = FALSE) +
-  theme_void()
+  coord_sf(xlim = c(min_lon, max_lon),
+           ylim = c(min_lat, max_lat),
+           expand = FALSE)
