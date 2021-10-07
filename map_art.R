72ea59569175f385f6024fba075857fd5170e32f
Sebastien Chaurin <sebastien.chaurin@gmail.com>
2021-10-07 10:11:02 +0200

    Add other locations and plot labels

--- map_art.R
+++ map_art.R
@@ -11,6 +11,43 @@ min_lat <- 47.81; max_lat <- 47.98
 bbx <- rbind(x = c(min_lon, max_lon), y = c(min_lat, max_lat))
 colnames(bbx) <- c("min", "max")
 
+saran <- c(1.915899, 47.951552, "Saran", "France")
+torreon <- c(-103.35533, 25.47737, "Torreon", "Mexico")
+waterloo <- c(-92.43332, 42.46813, "Waterloo", "USA")
+rosario <- c(-60.71211, -32.86711, "Rosario", "Argentina")
+tianjin <- c(117.72722, 39.07944, "Tianjin", "China")
+pune <- c(73.92919, 18.51553, "Pune", "India")
+springfield <- c(-93.20083, 37.24398, "Springfield", "USA")
+coffeyville <- c(-95.58388, 37.09781, "Coffeyville", "USA")
+moline <- c(-90.42295, 41.47645, "Moline", "USA")
+
+choice <- springfield
+
+center <- choice[1:2] %>% as.numeric()
+center_country <- choice[4]
+loc_lab <- paste0(choice[3], ", ", choice[4])
+GPS_lab <- paste0(if_else(center[2] < 0,
+                          paste0(-center[2] %>% round(2), "°S"),
+                          paste0(center[2] %>% round(2), "°N")),
+                  " / ",
+                  if_else(center[1] < 0,
+                          paste0(-center[1] %>% round(2), "°W"),
+                          paste0(center[1] %>% round(2), "°E")))
+
+size_lon <- 0.23
+size_lat <- 0.17
+buffer <- 0.1
+min_lon <- center[1] - size_lon
+max_lon <- center[1] + size_lon
+min_lat <- center[2] - size_lat
+max_lat <- center[2] + size_lat
+bbx <- rbind(x = c(min_lon, max_lon),
+             y = c(min_lat, max_lat))
+bbx_big <- rbind(x = c(min_lon - buffer, max_lon + buffer),
+             y = c(min_lat - buffer, max_lat + buffer))
+colnames(bbx) <- c("min", "max")
+colnames(bbx_big) <- c("min", "max")
+
 bbx <- getbb("brisbane australia")
 
 min_lon <- 152.7; max_lon <- 153.5
@@ -18,7 +55,7 @@ min_lat <- -27.7; max_lat <- -27
 bbx <- rbind(x = c(min_lon, max_lon), y = c(min_lat, max_lat))
 colnames(bbx) <- c("min", "max")
 
-highways <- bbx %>%
+highways <- bbx_big %>%
   opq() %>%
   add_osm_feature(key = "highway",
                   value = c("motorway", "trunk",
@@ -36,7 +73,7 @@ ggplot() +
           alpha = .65) +
   theme_void()
 
-streets <- bbx %>%
+streets <- bbx_big %>%
   opq() %>%
   add_osm_feature(key = "highway",
                   value = c("residential", "living_street",
@@ -67,27 +104,27 @@ ggplot() +
            expand = FALSE) +
   theme(legend.position = F) + theme_void()
 
-river <- bbx %>%
+river <- bbx_big %>%
   opq() %>%
   add_osm_feature(key = "waterway", value = "river") %>%
   osmdata_sf()
 
-lake <- bbx %>%
+lake <- bbx_big %>%
   opq() %>%
   add_osm_feature(key = "water", value = "lake") %>%
   osmdata_sf()
 
-railway <- bbx %>%
+railway <- bbx_big %>%
   opq() %>%
   add_osm_feature(key = "railway", value = "rail") %>%
   osmdata_sf()
 
-coast_data <- bbx %>%
+coast_data <- bbx_big %>%
   opq() %>%
   add_osm_feature(key = "natural", value = "coastline") %>%
   osmdata_sf()
 
-water <- bbx %>% 
+water <- bbx_big %>%
   opq() %>% 
   add_osm_feature(key = 'natural', value = 'water') %>% 
   osmdata_sf()
@@ -170,17 +207,92 @@ ggplot() +
   #         col = "red",
   #         size = 1,
   #         alpha = 0.5) +
-  coord_sf(xlim = c(min_lon, max_lon),
-           ylim = c(min_lat, max_lat),
-           expand = FALSE)
+  # LABEL UP
+# geom_text(aes(x = center[1],
+#               y = center[2] + 0.85 * size_lat),
+#           label = "John Deere",
+#           size = 18,
+#           fontface = "bold",
+#           family = "Roboto") +
+#   geom_text(aes(x = center[1],
+#                 y = center[2] + 0.73 * size_lat),
+#             label = loc_lab,
+#             size = 12,
+#             fontface = "bold",
+#             family = "Roboto") +
+#   geom_text(aes(x = center[1],
+#                 y = center[2] + 0.772 * size_lat),
+#             label = paste0("___",
+#                            rep(" ", loc_lab %>% stringr::str_length() + 14) %>%
+#                              stringr::str_c(collapse = ""),
+#                            "___"),
+#             size = 12,
+#             family = "Roboto") +
+#   geom_text(aes(x = center[1],
+#                 y = center[2] + 0.64 * size_lat),
+#             label = GPS_lab,
+#             size = 10,
+#             family = "Roboto") +
+  # LABEL Down
+  geom_text(aes(x = center[1],
+                y = center[2] - 0.65 * size_lat),
+            label = "John Deere",
+            size = 18,
+            fontface = "bold",
+            family = "Roboto") +
+  geom_text(aes(x = center[1],
+                y = center[2] - 0.772 * size_lat),
+            label = loc_lab,
+            size = 12,
+            fontface = "bold",
+            family = "Roboto") +
+  geom_text(aes(x = center[1],
+                y = center[2] - 0.73 * size_lat),
+            label = paste0("___",
+                           rep(" ", loc_lab %>% stringr::str_length() + 14) %>%
+                             stringr::str_c(collapse = ""),
+                           "___"),
+            size = 12,
+            family = "Roboto") +
+  geom_text(aes(x = center[1],
+                y = center[2] - 0.85 * size_lat),
+            label = GPS_lab,
+            size = 10,
+            family = "Roboto") +
+  geom_point(aes(x = center[1],
+                 y = center[2]),
+             shape = 21,
+             col = "#367C2B",
+             size = 5,
+             stroke = 3) +
+  geom_point(aes(x = center[1],
+                 y = center[2]),
+             shape = 21,
+             col = "#367C2B",
+             size = 15,
+             stroke = 3) +
+  geom_point(aes(x = center[1],
+                 y = center[2]),
+             shape = 21,
+             col = "#367C2B",
+             size = 25,
+             stroke = 3) +
+  coord_sf(
+    xlim = c(min_lon, max_lon),
+    ylim = c(min_lat, max_lat),
+    expand = FALSE
+  ) +
+  labs(x = "",
+       y = "")
 
-ggsave(last_plot(), 
-       filename = "Map_art.pdf",
-       scale = 1, 
-       width = 36, 
-       height = 24, 
-       units = "in",
-       dpi = 500)
+ggsave(last_plot(),
+  filename = paste0(choice[3], ".png"),
+  # scale = 1,
+  width = 17,
+  height = 15,
+  # units = "cm",
+  dpi = 500
+)
 
 
 
@@ -198,38 +310,126 @@ st_crs(pol) <- 4326
 pol %>% plot()
 
 multipol <- st_split(st_geometry(pol), st_geometry(blade))
-land <- st_cast(multipol[[1]][[1]], 'POLYGON') %>% st_geometry %>% st_sf
+land <- st_cast(multipol[[1]][[1]], "POLYGON") %>%
+  st_geometry() %>%
+  st_sf()
 st_crs(land) <- 4326
 land %>% plot()
-sea <- st_difference(pol, land) %>% st_geometry %>% st_sf
+sea <- st_difference(pol, land) %>%
+  st_geometry() %>%
+  st_sf()
 st_crs(sea) <- 4326
 sea %>% plot()
 
-blade <- water$osm_lines %>% st_union %>% st_line_merge
+blade <- water$osm_lines %>%
+  st_union() %>%
+  st_line_merge()
 blade %>% plot()
+ggplot() +
+  geom_sf(data = blade, fill = "red", alpha = 0.5) +
+  coord_sf(
+    xlim = c(min_lon, max_lon),
+    ylim = c(min_lat, max_lat),
+    expand = FALSE
+  )
+  
 multipol <- st_split(st_geometry(pol), st_geometry(blade))
 
-1:(multipol[[1]] %>% length()) %>% 
+st <- streets$osm_lines
+hi <- highways$osm_lines
+st_crs(st) <- 4326
+st_crs(hi) <- 4326
+
+1:(multipol[[1]] %>% length()) %>%
   purrr::walk(function(x) {
-    df <- st_cast(multipol[[1]][[x]], 'POLYGON') %>% st_geometry() %>% st_sf()
-    # st_crs(df) <- 4326
-    
+    df <- st_cast(multipol[[1]][[x]], "POLYGON") %>%
+      st_geometry() %>%
+      st_sf()
+    st_crs(df) <- 4326
+
     g <- ggplot() +
+      geom_sf(
+        data = st,
+        col = color_roads,
+        size = .4,
+        alpha = .65
+      ) +
+      geom_sf(
+        data = hi,
+        col = color_roads,
+        size = .6,
+        alpha = .8
+      ) +
       geom_sf(data = df, fill = "red", alpha = 0.5) +
-      coord_sf(xlim = c(min_lon, max_lon),
-               ylim = c(min_lat, max_lat),
-               expand = FALSE)
+      coord_sf(
+        xlim = c(min_lon, max_lon),
+        ylim = c(min_lat, max_lat),
+        expand = FALSE
+      ) +
+      labs(title = paste0("X = ", x))
     print(g)
   })
 
-sea <- st_cast(multipol[[1]][[3]], 'POLYGON') %>% st_union(st_cast(multipol[[1]][[2]], 'POLYGON')) %>% st_geometry %>% st_sf
+sea <- st_cast(multipol[[1]][[2]], "POLYGON") %>%
+  # st_union(st_cast(multipol[[1]][[3]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[4]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[5]], "POLYGON")) %>%
+  # st_union(st_cast(multipol[[1]][[6]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[7]], "POLYGON")) %>%
+  # st_union(st_cast(multipol[[1]][[8]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[9]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[10]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[11]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[12]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[13]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[14]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[15]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[16]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[17]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[18]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[19]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[20]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[21]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[22]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[23]], "POLYGON")) %>%
+  st_geometry() %>%
+  st_sf()
 st_crs(sea) <- 4326
 sea %>% plot()
 
-tmp <- st_cast(multipol[[1]][[1]], 'POLYGON') %>% st_geometry() %>% st_sf()
+land <- st_cast(multipol[[1]][[2]], "POLYGON") %>%
+  # st_union(st_cast(multipol[[1]][[3]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[4]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[5]], "POLYGON")) %>%
+  # st_union(st_cast(multipol[[1]][[6]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[7]], "POLYGON")) %>%
+  # st_union(st_cast(multipol[[1]][[8]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[9]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[10]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[11]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[12]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[13]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[14]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[15]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[16]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[17]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[18]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[19]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[20]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[21]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[22]], "POLYGON")) %>%
+  st_union(st_cast(multipol[[1]][[23]], "POLYGON")) %>%
+  st_geometry() %>%
+  st_sf()
+st_crs(land) <- 4326
+land %>% plot()
+
+tmp <- st_cast(multipol[[1]][[1]], "POLYGON") %>%
+  st_geometry() %>%
+  st_sf()
 st_crs(tmp) <- 4326
 
-water_bodies <- st_difference(pol, tmp) %>% st_geometry %>% st_sf
-st_crs(sea) <- 4326
-
-
+water_bodies <- st_difference(pol, tmp) %>%
+  st_geometry() %>%
+  st_sf()
+st_crs(water_bodies) <- 4326
